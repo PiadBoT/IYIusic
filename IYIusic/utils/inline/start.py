9ddea389acfe275a1c37b886bab7142b983aa42c
PiadBoT <ambotteam@gmail.com>
2024-09-15 10:39:38 +0530

    Update start.py

--- IYIusic/utils/inline/start.py
+++ IYIusic/utils/inline/start.py
@@ -10,7 +10,9 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users+"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+        ],
+        [
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")
         ],
     ]
     return buttons
@@ -27,10 +29,9 @@ def private_panel(_):
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
+            InlineKeyboardButton(text=_["ST_B_3"], callback_data="LG")
         ],
     ]
     return buttons
